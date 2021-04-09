8df9917e5ec5aa314d20ba0832d0cc05048505e4
Isaac Olawale <bodolawale@gmail.com>
2021-04-09 16:39:28 +0100

    added validate url

--- services/appService.js
+++ services/appService.js
@@ -1,10 +1,24 @@
+const { nanoid } = require("nanoid");
 class AppService {
 	static async shortenURL(url) {
+		if (!AppService.validateURL(url)) throw new Error("Invalid URL");
+
+		const shortId = nanoid(6);
 		return {
 			url,
 			host: "some host",
-			path: "some path",
+			path: shortId,
 		};
+	}
+
+	static validateURL(url) {
+		try {
+			new URL(url);
+		} catch (e) {
+			console.error(e);
+			return false;
+		}
+		return true;
 	}
 }
 
