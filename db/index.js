261937608edf0fcfaa33ec1164ab023eb65c99a7
Isaac Olawale <bodolawale@gmail.com>
2021-04-10 13:53:13 +0100

    changed ssl

--- db/index.js
+++ db/index.js
@@ -2,7 +2,10 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialectOptions: {
-		ssl: true,
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
 	},
 });
 
